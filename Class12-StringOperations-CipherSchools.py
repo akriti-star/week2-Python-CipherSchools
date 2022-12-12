c3ddfd3ac3179b3bacaba01d95bc1d680f162254
Akriti Sharma <114270752+akriti-star@users.noreply.github.com>
2022-12-12 22:50:06 +0530

    Add files via upload

---
+++ Class12-StringOperations-CipherSchools.py
@@ -0,0 +1,17 @@
+a=5
+print("value of a is %d"%(a))
+print("value of a is{}".format(a))
+#
+a,b,c,d=1,2,3,4
+print("a={2},b={1},c={0}".format(c,b,a))
+#
+name="Akrti" 
+company="shuttl"
+print("Hello {name} welcome to my facebook".format(name=name,company=company))
+print(f"name={name}")
+#replace
+print("Akriti".replace("i","e"))
+#coumt
+print("Akriti".count("i"))
+#split
+print("1,2,3,4,5".split(","))
